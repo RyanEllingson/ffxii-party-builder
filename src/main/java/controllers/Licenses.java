26aa53b348fb6e56fcea4ee58eca9546ac354efe
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-11-25 18:55:31 -0600

    changed getLicensesByRegion route to return a LicenseList

--- src/main/java/controllers/Licenses.java
+++ src/main/java/controllers/Licenses.java
@@ -2,6 +2,8 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +12,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import dao.Dao;
 import dao.DaoImpl;
+import models.Region;
 import util.ConnectionFactory;
+import util.LicenseList;
 
 public class Licenses {
 
@@ -20,7 +24,9 @@ public class Licenses {
 			Dao dao = new DaoImpl(conn);
 			ObjectMapper om = new ObjectMapper();
 			String[] params = req.getRequestURI().split("/");
-			res.getWriter().write(om.writeValueAsString(dao.getLicensesByRegion(Integer.parseInt(params[4]))));
+			List<Region> regionList = new ArrayList<>();
+			regionList.add(dao.getRegionById(Integer.parseInt(params[4])));
+			res.getWriter().write(om.writeValueAsString(new LicenseList(regionList)));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
